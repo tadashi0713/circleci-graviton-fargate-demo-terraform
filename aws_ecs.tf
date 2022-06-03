274ae94203252b73d6437f3b7907f90c2dffbf36
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2022-06-03 19:43:52 +0900

    Until create fargate

---
+++ aws_ecs.tf
@@ -0,0 +1,31 @@
+resource "aws_ecs_task_definition" "task" {
+  family                   = "tadashi-fargate-graviton-demo-task"
+  cpu                      = "256"
+  memory                   = "512"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  container_definitions    = file("./container_definitions.json")
+}
+
+resource "aws_ecs_cluster" "cluster" {
+  name = "tadashi-fargate-graviton-demo-cluster"
+}
+
+resource "aws_ecs_service" "service" {
+  name                              = "tadashi-fargate-graviton-demo-service"
+  cluster                           = aws_ecs_cluster.cluster.arn
+  task_definition                   = aws_ecs_task_definition.task.arn
+  desired_count                     = 1
+  launch_type                       = "FARGATE"
+  platform_version                  = "1.4.0"
+
+  network_configuration {
+    assign_public_ip = true
+    security_groups  = [aws_security_group.service.id]
+    subnets = module.vpc.public_subnets
+  }
+
+  lifecycle {
+    ignore_changes = [task_definition]
+  }
+}
